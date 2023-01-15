ec0a185e623ae5a7556e8e1693648f736638e794
abagheri@ifschain.com <info@alirezanahri.ir>
2023-01-15 15:01:03 +0330

    server Dns and read from file config

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/miekg/dns"
+)
+
+var records = map[string]string{
+	"test.service.": "192.168.0.2",
+	"test.com.":     "192.168.1.1",
+}
+
+func parseQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			log.Printf("Query for %s\n", q.Name)
+			ip := records[q.Name]
+			if ip != "" {
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				}
+			}
+		}
+	}
+}
+
+func ReadConfigfile(ConfigName string) map[string]interface{} {
+	var configJson map[string]interface{}
+	configfile, err := os.Open(ConfigName)
+
+	if err != nil {
+		log.Println(err)
+	}
+	jsonResult, err := io.ReadAll(configfile)
+	defer configfile.Close()
+	err = json.Unmarshal([]byte(jsonResult), &configJson)
+	if err != nil {
+		log.Println(err)
+	}
+	return configJson
+}
+func handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		parseQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
+func main() {
+	var ConfigJson map[string]interface{}
+	// attach request handler func
+	dns.HandleFunc(".", handleDnsRequest)
+	ConfigJson = ReadConfigfile("config.conf")
+
+	// start server
+	port := ConfigJson["Port"].(int)
+	protocol := ConfigJson["Protocol"].(string)
+	server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: protocol}
+	log.Printf("Starting at %d\n", port)
+	err := server.ListenAndServe()
+	defer server.Shutdown()
+	if err != nil {
+		log.Fatalf("Failed to start server: %s\n ", err.Error())
+	}
+}
